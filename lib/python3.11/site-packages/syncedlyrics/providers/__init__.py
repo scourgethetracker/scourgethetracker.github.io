d5d57ba7a59266a1ba41fe85b8f9628eae19a0d2
scourgethetracker <scourgethetracker@proton.me>
2023-11-22 18:11:38 -0800

    pyvenv lib directory

---
+++ lib/python3.11/site-packages/syncedlyrics/providers/__init__.py
@@ -0,0 +1,7 @@
+"""Synchronized Lyrics providers"""
+
+from .netease import NetEase
+from .deezer import Deezer
+from .lyricsify import Lyricsify
+from .megalobiz import Megalobiz
+from .musixmatch import Musixmatch
